fd2ad8f14dd4b8690d552eab5ee4c8f45fd59c73
Karl Dreher <karldreher@outlook.com>
2025-01-22 20:53:42 -0800

    test: add pytest options and more coverage

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,8 @@
 from mixtapematrix.main import MixtapeMatrix, cli
+from mixtapematrix.config import ConfigFile
 import click
+import pytest
+import yaml
 
 def test_config(mkdirs):
     matrix = MixtapeMatrix("test/matrix.yaml")
@@ -8,6 +11,14 @@ def test_config(mkdirs):
     assert config
     assert config.matrix[0].source.path == "test/source"
     assert config.matrix[0].destination.path == "test/output"
+
+def test_invalid_config():
+    with pytest.raises(ValueError):
+        matrix = MixtapeMatrix("test/invalid.yaml")
+    with pytest.raises(ValueError):
+        with open("test/invalid.yaml", "r") as f:
+            # same as above, but more directly catching the error we expect
+            matrix = ConfigFile.model_validate(yaml.safe_load(f))
 
 def test_cli():
     assert type(cli) == click.core.Command
