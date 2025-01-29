c052fb16a76937641bd06a93ce30848ad6bcface
Karl Dreher <karldreher@outlook.com>
2025-01-28 20:25:48 -0800

    feat: filter

--- mixtapematrix/routers/mp3_router.py
+++ mixtapematrix/routers/mp3_router.py
@@ -1,8 +1,9 @@
 from typing import Generator
+
 import eyed3
 
-from .files import FileRouter, File, search_files
 from ..config import MatrixConfig
+from .files import File, FileRouter, search_files
 
 
 class TagRouter(FileRouter):
@@ -28,20 +29,22 @@ class TagRouter(FileRouter):
         for i in self.matrix_config.mp3_files:
             # Dynamically search for the tag in the MP3 file
             for k, v in i.items():
-                for file_path in search_files(source_path, exclude_path):
-                    if file_path.endswith(".mp3"):
-                        try:
-                            audiofile = eyed3.load(path=file_path)
-                        except Exception as e:
-                            print(f"Error loading {file_path}: {e}")
-                            continue
-                        if k == "genre":
-                            # Genre is a special case
-                            # We most likely want to support just a few select tags, but genre should be one
-                            if audiofile.tag.genre.name.lower() == v.lower():
-                                yield File(path=file_path)
-
-                        # All other tags, but primarily geared toward artist / album
-                        audiofile_tag = getattr(audiofile.tag, k)
-                        if str(audiofile_tag).lower() == v.lower():
+                for file_path in filter(
+                    lambda x: x.endswith(".mp3"),
+                    search_files(source_path, exclude_path),
+                ):
+                    try:
+                        audiofile = eyed3.load(path=file_path)
+                    except Exception as e:
+                        print(f"Error loading {file_path}: {e}")
+                        continue
+                    if k == "genre":
+                        # Genre is a special case
+                        # We most likely want to support just a few select tags, but genre should be one
+                        if audiofile.tag.genre.name.lower() == v.lower():
                             yield File(path=file_path)
+
+                    # All other tags, but primarily geared toward artist / album
+                    audiofile_tag = getattr(audiofile.tag, k)
+                    if str(audiofile_tag).lower() == v.lower():
+                        yield File(path=file_path)
